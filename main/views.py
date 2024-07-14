d940aa17f6271920b8323d0106c657a45ea9cd6a
alimehdi <alimehdi9651@gmail.com>
2024-07-14 17:52:25 +0530

    ..

--- main/views.py
+++ main/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        user = authenticate(username=username, password=password)
+        if user:
+             login(request, user)
+             return redirect(request, 'login.html')
+             
+    return render(request, 'login.html')
+def register_view(request):
+         if request.method == 'POST':
+              username = request.POST.get('username')
+              password = request.POST.get('password')
+              email = request.POST.get('email')
+              confirmpassword = request.POST.get('confirm-password')
+              print(username, email,password, confirmpassword)
+              if password == confirmpassword:
+                   user = User(username = username, email= email)
+                   user.set_password(password)
+                   user.save()
+                   return redirect('login')
+            
+         return render(request, 'register.html')
+ 
